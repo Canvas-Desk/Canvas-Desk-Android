23277a4d5e35a4fb1385e0131bbaba2d1645432a
Rohan Pandit <pandit.rohan@gmail.com>
2015-02-06 23:49:06 -0500

    checkboxes yay

--- app/src/main/java/org/rohanp/layerpicker/MainActivity.java
+++ app/src/main/java/org/rohanp/layerpicker/MainActivity.java
@@ -1,12 +1,18 @@
 package org.rohanp.layerpicker;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+    ArrayList<CheckBox> list = new ArrayList<CheckBox>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,17 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void addLayer(View view){
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.mainLayout);
+        CheckBox c = new CheckBox(this);
+        list.add(c);
+        int checkBoxNumber = list.size();
+        c.setText("Layer " + checkBoxNumber);
+        layout.addView(c);
+
+
     }
 
     @Override
